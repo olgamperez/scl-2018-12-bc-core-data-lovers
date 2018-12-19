56255a0c51fb52af4f84beef9ea10bec9348bcaa
LeslieSaVa <leslie.sabattin@usach.cl>
2018-12-19 08:55:44 -0300

    actualizando cambios de prueba

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let nombres =[];
+
+for (let i=0; i<data.length;i++){
+    console.log(data[i].name);
+    //console.log(data[i]["name"]);
+}
+
+console.log(nombres)
+
+//´<img src="${POKEMON.pokemon[1].img}" alt="">
+//document.getElementById(`root`).innerHTML= ´<img src=" ´ + POKEMON.pokemon[1].img ´ " alt=""></img>´
+
