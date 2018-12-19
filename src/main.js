d02030bc0b84f9f1c5ccde4f22bb3fad16a770ed
olgamperez <ing.olgaperez@gmail.com>
2018-12-19 20:36:09 -0300

    Agregando funcion filtrar por nombre y funcion boton

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-let nombres =[];
-
+/*let nombres =[];
 for (let i=0; i<data.length;i++){
     console.log(data[i].name);
     //console.log(data[i]["name"]);
@@ -10,4 +9,11 @@ console.log(nombres)
 //´<img src="${POKEMON.pokemon[1].img}" alt="">
 //document.getElementById(`root`).innerHTML= ´<img src=" ´ + POKEMON.pokemon[1].img ´ " alt=""></img>´
 
-
+for (let i=0; i<dataPokemon.length; i++){
+    console.log(dataPokemon[i].type);}*/
+//document.write(resultNameFilter);
+document.getElementById("btn").addEventListener("click", () => {
+   for (let i=0; i < result.length; i++) {
+       document.getElementById("root").innerHTML += (result)[i];
+   }
+});
