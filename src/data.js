41159e948ac950c923c3880412b8b2bf6c5df1e6
olgamperez <ing.olgaperez@gmail.com>
2018-12-23 16:08:24 -0300

    SortData funcionando y Historias de usuario a README

--- src/data.js
+++ src/data.js
@@ -1,19 +1,103 @@
-/*Todo lo que es filtrar ,
-esta es una función de ejemplo
-puedes ver como agregamos la función a nuestro objeto global window;*/
-//window.example= example;
 
-const dataPokemon = POKEMON.pokemon;
+//window.example= example;
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+/*const example = () => {
+ return ‘example’;
+};*/
+
+window.POKEMON= {
+  sortData: (data, sortBy, sortOrder) => {
+
+  const dataPokemon = data
+
+  if (sortBy === "name" && sortOrder === "az"){
+    dataPokemon.sort((prev, next)=> {
+ 
+      if (prev.name > next.name) {
+        return 1;
+      }
+      if (prev.name < next.name) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "name" && sortOrder === "za"){
+    dataPokemon.sort((prev, next)=> {
+ 
+      if (prev.name < next.name) {
+        return 1;
+      }
+      if (prev.name > next.name) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "num" && sortOrder === "small-big"){
+    dataPokemon.sort((prev, next)=> {
+ 
+      if (prev.num > next.num) {
+        return 1;
+      }
+      if (prev.num < next.num) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "num" && sortOrder === "big-small"){
+    dataPokemon.sort((prev, next)=> {
+ 
+      if (prev.num < next.num) {
+        return 1;
+      }
+      if (prev.num > next.num) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  return dataPokemon
+}
+} 
+ 
+/*
+  sortData(dataPokemon,namesPokemon, sortOrder)=> {
+  let namesPokemon = []
+  for (let i=0; i<dataPokemon.length; i++) {
+  namesPokemon.push(dataPokemon[i].name)
+};
+resultado(namesPokemon.sort())
+}
+return resultado;
+
+  
+
+//Aqui se declara la variable para Ordenar de Z-A
+//const dataPokemonReverse = POKEMON.pokemon;
+//  let namesPokemonReverse = []
+//  for (let i=0; i<dataPokemonReverse.length; i++) {
+//  namesPokemonReverse.push(dataPokemon[i].name)
+//};
+//console.log(namesPokemonReverse.sort().reverse());
+//
+//
+//}
+
+
+
+
+/*const dataPokemon = POKEMON.pokemon;
   let namesPokemon = []
   for (let i=0; i<dataPokemon.length; i++) {
   namesPokemon.push(dataPokemon[i].name)
 };
 //console.log(namesPokemon);
 
-let result = namesPokemon.filter(Bulbasaur1 => Bulbasaur1 === "Pikachu");
-console.log(String(result));
-
-
+let result = namesPokemon.onchange(Bulbasaur1 => Bulbasaur1 === "Pikachu");
+console.log(String(result));*/
 
   //console.log(nombres) me arroja un array con los nombres*/
   // Para acceder a la imagen  POKEMON.pokemon[1].img
--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
-/*let nombres =[];
-for (let i=0; i<data.length;i++){
-    console.log(data[i].name);
-    //console.log(data[i]["name"]);
-}
-
-console.log(nombres)
 
 //´<img src="${POKEMON.pokemon[1].img}" alt="">
 //document.getElementById(`root`).innerHTML= ´<img src=" ´ + POKEMON.pokemon[1].img ´ " alt=""></img>´
 
-for (let i=0; i<dataPokemon.length; i++){
-    console.log(dataPokemon[i].type);}*/
 //document.write(resultNameFilter);
-document.getElementById("btn").addEventListener("click", () => {
-   for (let i=0; i < result.length; i++) {
-       document.getElementById("root").innerHTML += (result)[i];
-   }
+//document.getElementById("btn").addEventListener("click", () => {
+//   for (let i=0; i < result.length; i++) {
+//       document.getElementById("root").innerHTML += (result)[i];
+//   }
+//});
+
+document.getElementById("option-order").addEventListener("change", () => {
+    document.getElementById("root").innerHTML = "";
+    if (document.getElementById("option-order").value === "az" || document.getElementById("option-order").value === "za"){
+        for(let i=0; i <POKEMON.pokemon.length; i++){
+            document.getElementById("root").innerHTML += `${window.POKEMON.sortData(POKEMON.pokemon, "name", document.getElementById("option-order").value)[i].name} <br>`;
+        }
+    }
+    if(document.getElementById("option-order").value === "small-big" || document.getElementById("option-order").value === "big-small") {
+        for(let i=0; i <POKEMON.pokemon.length; i++){
+            document.getElementById("root").innerHTML += `${window.POKEMON.sortData(POKEMON.pokemon, "num", document.getElementById("option-order").value)[i].num} <br>`;
+        }
+    }
 });
