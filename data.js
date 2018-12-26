7f33f64cb8e366889d4e73f52290d8b97db36f74
LeslieSaVa <leslie.sabattin@usach.cl>
2018-12-26 17:42:29 -0300

    cogo con las funciones filtrar y ordenar OK

--- data.js
+++ data.js
@@ -1,27 +1,62 @@
-//Todo lo que es filtrar ,
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
-
-window.funciones = {
+window.pokemones = {
 
 filterData: (data,condition) => {
   const dataPokemon = data;
   const pokemonGrass = dataPokemon.filter(pokemon => {
     return pokemon.type.indexOf(condition) !== -1 ;
   })
-  console.log(pokemonGrass)
   return pokemonGrass
+ },
+
+sortData: (data, sortBy, sortOrder) => {
+  const dataPokemon = data
+  if (sortBy === "name" && sortOrder === "az"){
+    dataPokemon.sort((prev, next)=> {
+      if (prev.name > next.name) {
+        return 1;
+      }
+      if (prev.name < next.name) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "name" && sortOrder === "za"){
+    dataPokemon.sort((prev, next)=> {
+      if (prev.name < next.name) {
+        return 1;
+      }
+      if (prev.name > next.name) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "num" && sortOrder === "small-big"){
+    dataPokemon.sort((prev, next)=> {
+      if (prev.id > next.id) {
+        return 1;
+      }
+      if (prev.id < next.id) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  if (sortBy === "num" && sortOrder === "big-small"){
+    dataPokemon.sort((prev, next)=> {
+      if (prev.id < next.id) {
+        return 1;
+      }
+      if (prev.id > next.id) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  return dataPokemon
  }
 }
-
-
-
 
 
 //let pokemonGrass = pokemonType.filter(tipo => {
--- main.js
+++ main.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", function(){
   M.AutoInit();
 });
 
+
 document.getElementById("select-type-pokemon").addEventListener("change",(evento)=>{
   evento.preventDefault();
   let type = document.getElementById("select-type-pokemon").value;
   fetch ("data/pokemon/pokemon.json")
-  .then(data1 => data1.json())
-  .then(data1 => { 
-    tabla(funciones.filterData(data1.pokemon,type))
+    .then(data1 => data1.json())
+    .then(data1 => { 
+      tabla(pokemones.filterData(data1.pokemon,type))
  })
 })
-
 
 const contenido = document.querySelector("#contenido")
   fetch ("data/pokemon/pokemon.json")
@@ -61,9 +61,57 @@ const contenido = document.querySelector("#contenido")
       } 
       }
   }
+  document.getElementById("option-order").addEventListener("change", () => {
+    contenido.innerHTML = "";
+    if (document.getElementById("option-order").value === "az" || document.getElementById("option-order").value === "za"){
+      fetch ("data/pokemon/pokemon.json")
+      .then(data => data.json())
+      .then(data=>{
+ 
+        for (let valor of pokemones.sortData(data.pokemon,"name",document.getElementById("option-order").value)){
+          //console.log(valor.img)
+          contenido.innerHTML +=`
+            <div class="col s6 m3">
+              <div class="card">
+                <div class="card-image">
+                  <img class="responsive-img" src= "${valor.img}">
+                </div>
+                <div class="card-content">
+                  <p>#${valor.num}</p>
+                  <span class="card-title">${valor.name}</span>
+                  <p>${valor.type}</p>
+                </div>
+              </div>
+            </div>
+          `
+    }
+  })
+ }
+ if (document.getElementById("option-order").value === "small-big" || document.getElementById("option-order").value === "big-small"){
+  fetch ("data/pokemon/pokemon.json")
+  .then(data => data.json())
+  .then(data=>{
+ 
+    for (let valor of pokemones.sortData(data.pokemon,"num",document.getElementById("option-order").value)){
+      //console.log(valor.img)
+      contenido.innerHTML +=`
+        <div class="col s6 m3">
+          <div class="card">
+            <div class="card-image">
+              <img class="responsive-img" src= "${valor.img}">
+            </div>
+            <div class="card-content">
+              <p>#${valor.num}</p>
+              <span class="card-title">${valor.name}</span>
+              <p>${valor.type}</p>
+            </div>
+          </div>
+        </div>
+      `
+ }
+ })
+ }
+ })
 
-  
 
-
-      
 
