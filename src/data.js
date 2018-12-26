65847a590c8edbbd3c111c775e1e8c9759a92397
LeslieSaVa <leslie.sabattin@usach.cl>
2018-12-26 15:01:18 -0300

    funciona el filtrado hay que agregar la función ordenar

--- src/data.js
+++ src/data.js
@@ -8,6 +8,42 @@ const example = () => {
 
 window.example = example;
 
+window.funciones = {
+
+filterData: (data,condition) => {
+  const dataPokemon = data;
+  const pokemonGrass = dataPokemon.filter(pokemon => {
+    return pokemon.type.indexOf(condition) !== -1 ;
+  })
+  console.log(pokemonGrass)
+  return pokemonGrass
+ }
+}
+
+
+
+
+
+//let pokemonGrass = pokemonType.filter(tipo => {
+      //return tipo.indexOf("Grass") !== -1;
+    //});
+  //}
+ //}
+
+
+
+  //const dataPokemon = POKEMON.pokemon;
+  //let pokemonType =[];
+  //for (let i=0; i < dataPokemon.length; i++){
+    //pokemonType.push(dataPokemon[i].type);
+  //}
+  //console.log(pokemonType);
+ //obtengo todos los pokemones tipo Grass 
+//let pokemonGrass = pokemonType.filter(tipo => {
+   //return tipo.indexOf("Grass") !== -1;
+
+//});
+//console.log(pokemonGrass)
 
 //for (let i=0; i<data.length; i++){ console.log(data[i])} muestra lo que tengo dentro de cada objeto
 //for (let i=0; i<data.length; i++){ console.log(data[i].name)} muestra solo los nombres de cada objeto
@@ -19,4 +55,21 @@ window.example = example;
   }
   console.log(nombres) me arroja un array con los nombres*/
   // Para acceder a la imagen  POKEMON.pokemon[1].img
+
+//const data = POKEMON.pokemon 
+
+
+//console.log(data);
+//nombrePokemones(data) // asi se ejecuta automaticamente
+
+//const nombrePokemones =(data) => {
+  //console.log(data)
+  //let nombres = [];
+  //for(let i=0; i<data.lenght; i++){
+    //nombres.push(data[i].name);
+  //}
+  //console.log()
+  //return nombres;
+//}
+//window.nombrePokemones = nombrePokemones;
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,69 @@
-let nombres =[];
+//Initialization for Components 
+document.addEventListener("DOMContentLoaded", function(){
+  M.AutoInit();
+});
 
-for (let i=0; i<data.length;i++){
-    console.log(data[i].name);
-    //console.log(data[i]["name"]);
-}
+document.getElementById("select-type-pokemon").addEventListener("change",(evento)=>{
+  evento.preventDefault();
+  let type = document.getElementById("select-type-pokemon").value;
+  fetch ("data/pokemon/pokemon.json")
+  .then(data1 => data1.json())
+  .then(data1 => { 
+    tabla(funciones.filterData(data1.pokemon,type))
+ })
+})
 
-console.log(nombres)
 
-//´<img src="${POKEMON.pokemon[1].img}" alt="">
-//document.getElementById(`root`).innerHTML= ´<img src=" ´ + POKEMON.pokemon[1].img ´ " alt=""></img>´
+const contenido = document.querySelector("#contenido")
+  fetch ("data/pokemon/pokemon.json")
+  .then(data => data.json())
+  .then(data => {
+      tabla(data)
+   })
 
+  function tabla(data){
+    contenido.innerHTML="";
+      if (Array.isArray(data)){
+        for (let valor of data){
+          //console.log(valor.img)
+          contenido.innerHTML +=`
+            <div class="col s6 m3">
+              <div class="card">
+                <div class="card-image">
+                  <img class="responsive-img" src= "${valor.img}">
+                </div>
+                <div class="card-content">
+                  <p>#${valor.num}</p>
+                  <span class="card-title">${valor.name}</span>
+                  <p>${valor.type}</p>
+                </div>
+              </div>
+            </div> 
+          `
+      }
+      }else{
+      for (let valor of data.pokemon){
+        //console.log(valor.img)
+        contenido.innerHTML +=`
+          <div class="col s6 m3">
+            <div class="card">
+              <div class="card-image">
+                <img class="responsive-img" src= "${valor.img}">
+              </div>
+              <div class="card-content">
+                <p>#${valor.num}</p>
+                <span class="card-title">${valor.name}</span>
+                <p>${valor.type}</p>
+              </div>
+            </div>
+          </div> 
+        `
+      } 
+      }
+  }
+
+  
+
+
+      
 
