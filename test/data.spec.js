e34f68ba13775ad504be4135b4d7b344230addcd
olgamperez <ing.olgaperez@gmail.com>
2018-12-30 17:35:25 -0300

    Agregando información al readme

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,28 +1,9 @@
 require('../src/data.js');
 
-
-describe('pokemones', () => {
+//intentando test
+describe('pokemon', () => {
+  it('Debería ser un objeto', () => {
+  expect(typeof pokemonFunction).toBe('object');
+  });
+  });
   
-  it('is a function', () => {
-    expect(typeof pokemones).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-//describe('cipher', () => {
-
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
-  describe('cipher.encode', () => {
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',() => { 
-    assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG"); //assert.equal(actual, expected)
-    });
-    it('debería retornar "0123456789" para "1234567890" con offest 9',() => { 
-      assert.equal(cipher.encode(9,"1234567890"), "0123456789");
-      });
