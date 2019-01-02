b3dda846db5b8c220bb18f4ad16d0725299289e5
olgamperez <ing.olgaperez@gmail.com>
2019-01-02 10:46:38 -0300

    Test para arreglo de objetos con los pokemones ordenados por name de forma A-Z

--- test/data.spec.js
+++ test/data.spec.js
@@ -5,6 +5,23 @@ describe('pokemon', () => {
   it('Debería ser un objeto', () => {
   expect(typeof window.pokemones).toBe('object');
   });
+  
+    
+  describe('pokemon.filterData', () => {
+    it('Debería ser una función', () => {
+    expect(typeof window.pokemones.filterData).toBe('function');
+    });
+  
+
   });
 
-  
+  describe('pokemon.sortData', () => {
+    it('Debería ser una función', () => {
+    expect(typeof window.pokemones.sortData).toBe('function');
+    });
+
+   it('Debería retornar un arreglo de objetos con los pokemones ordenados por "name" de forma A-Z', () => {
+     expect(typeof window.pokemones.sortData("az",["Wartortle", "Venasaur", "Bulbasaur", "Pinsir"], ["Bulbasaur", "Pinsir", "Venasaur", "Wartortle"]));
+    });
+  });
+});
