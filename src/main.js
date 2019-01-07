85f0b79c888d4d18a205cb8d7c49370e6a766f53
olgamperez <ing.olgaperez@gmail.com>
2019-01-07 15:24:17 -0300

    Test de tareas filterData y sortData listos

--- src/main.js
+++ src/main.js
@@ -1,121 +1,183 @@
-//Initialization for Components 
+//Initialization for Components Materialize
 document.addEventListener("DOMContentLoaded", function(){
   window.M.AutoInit();
-});
-
-//Volver al inicio apretando inicio en el menú de nav
-document.getElementById("back-to-initial").addEventListener("click", () => {
-  document.location = "index.html";
-})
-
-
-//Manejo de la función filtrar
-document.getElementById("select-type-pokemon").addEventListener("change",(evento)=>{
-  evento.preventDefault();
-  let type = document.getElementById("select-type-pokemon").value;
-  fetch ("data/pokemon/pokemon.json")
-    .then(data1 => data1.json())
-    .then(data1 => { 
-      tabla(window.pokemones.filterData(data1.pokemon,type))
+ });
+ //Volver al inicio apretando inicio en el menú de nav
+ document.getElementById("back-to-initial").addEventListener("click", () => {
+   document.location = "index.html";
  })
-})
-
-const contenido = document.querySelector("#contenido")
-  fetch ("data/pokemon/pokemon.json")
-  .then(data => data.json())
-  .then(data => {
-      tabla(data)
-   })
-
-  function tabla(data){
-    contenido.innerHTML="";
-      if (Array.isArray(data)){
-        for (let valor of data){
-          //console.log(valor.img)
-          contenido.innerHTML +=`
-            <div class="col s6 m3">
-              <div class="card">
-                <div class="card-image">
-                  <img class="responsive-img" src= "${valor.img}">
-                </div>
-                <div class="card-content">
-                  <p>#${valor.num}</p>
-                  <span class="card-title">${valor.name}</span>
-                  <p>${valor.type}</p>
-                </div>
-              </div>
+ let allPokemon;
+ const content = document.querySelector("#content")
+   fetch ("data/pokemon/pokemon.json")
+   .then(data => data.json())
+   .then(data => {
+       allPokemon = data;
+       runData();
+    })
+ //Click en la imagen de cada pokemon
+ let pokemonImages = document.getElementsByClassName("img-pokemon");
+ function clickOnImg (data) {
+     for (let i = 0; i<pokemonImages.length; i++) {
+       pokemonImages[i].addEventListener("click",() =>{
+         document.getElementById("first-page").style.display="none";
+         document.getElementById("second-page").style.display="block";
+        // const second = document.querySelector("#second-page")
+         document.getElementById("second-page").innerHTML +=`
+         <div id="name-number" class="card-panel teal grey"><span class="white-text" ><p class="flow-text">${data[i].name} Nº ${data[i].num}</p></span></div>
+         <div class="container">
+         <div class="row">
+             <div class="col s12 m6">
+               <div class="card">
+                 <div id="card-profile" class="card-image">
+                   <img  class="responsive-img" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${data[i].num}.png" alt="pokemon bulbasaur">
+                 </div>
+                 <div id="card-type" class="card-action">
+                   <span class="white-text" >
+                     <table  id="table-type">
+                        <h5 id="card-title">Tipo</h5>
+                         <tr>
+                           <td><div class="${data[i].type[0]}">${data[i].type[0]}</div></td>
+                           <td><div class="${data[i].type[1]}">${data[i].type[1]}</div></td>
+                         </tr>
+                     </table> 
+                   </span>   
+                 </div>
+               </div>
+             </div>
+            <div class="col s12 m6" >
+               <div class="card-panel teal grey center-align">
+                 <span class="white-text" >
+                   <table  id="table-info">
+                    <tr>
+                      <th>TIEMPO APARICIÓN</td>
+                      <th>ALTURA</td>
+                    </tr>
+                    <tr>
+                      <td>${data[i].spawn_time} min </td>
+                      <td>${data[i].height}</td>
+                    </tr>
+                    <tr>
+                      <th>PESO</th>
+                      <th>EGG</th>
+                    </tr>
+                    <tr>
+                      <td>${data[i].weight}</td>
+                      <td>${data[i].egg}</td>
+                    </tr>
+                   </table>
+                 </span>
+               </div>
             </div> 
-          `
-      }
-      }else{
-      for (let valor of data.pokemon){
-        //console.log(valor.img)
-        contenido.innerHTML +=`
-          <div class="col s6 m3">
-            <div class="card">
-              <div class="card-image">
-                <img class="responsive-img" src= "${valor.img}">
-              </div>
-              <div class="card-content">
-                <p>#${valor.num}</p>
-                <span class="card-title">${valor.name}</span>
-                <p>${valor.type}</p>
-              </div>
-            </div>
-          </div> 
-        `
-      } 
-      }
-  }
-  document.getElementById("option-order").addEventListener("change", () => {
-    contenido.innerHTML = "";
-    if (document.getElementById("option-order").value === "az" || document.getElementById("option-order").value === "za"){
-      fetch ("data/pokemon/pokemon.json")
-      .then(data => data.json())
-      .then(data=>{
- 
-        for (let valor of window.pokemones.sortData(data.pokemon,"name",document.getElementById("option-order").value)){
-          //console.log(valor.img)
-          contenido.innerHTML +=`
-            <div class="col s6 m3">
-              <div class="card">
-                <div class="card-image">
-                  <img class="responsive-img" src= "${valor.img}">
-                </div>
-                <div class="card-content">
-                  <p>#${valor.num}</p>
-                  <span class="card-title">${valor.name}</span>
-                  <p>${valor.type}</p>
-                </div>
-              </div>
-            </div>
-          `
-    }
+            <div class="col s12 m6" >
+               <div id="card-panel" class="card-panel teal grey center-align">
+                 <span class="white-text" >
+                   <table id="table-weaknesses">
+                     <h5>Debilidades</h5>               
+                    <tr>
+                      <td ><div class="${data[i].weaknesses[0]}">${data[i].weaknesses[0]}</div> </td>
+                      <td ><div class="${data[i].weaknesses[1]}">${data[i].weaknesses[1]}</div> </td>
+                    </tr>
+                    <tr>
+                       <td ><div class="${data[i].weaknesses[2]}">${data[i].weaknesses[2]}</div> </td>
+                       <td ><div class="${data[i].weaknesses[3]}">${data[i].weaknesses[3]}</div> </td>
+                    </tr>
+                    <tr>
+                       <td ><div class="${data[i].weaknesses[4]}">${data[i].weaknesses[4]}</div> </td>
+                       <td ><div class="${data[i].weaknesses[5]}">${data[i].weaknesses[5]}</div> </td>
+                   </tr>
+                  </table>
+                 </span>
+               </div>
+            </div>    
+           </div>
+   
+           <div><h4>Evoluciones</h4></div>
+           <div id="btn-go-back">
+               <a href="" class="waves-effect waves-light btn">Volver al Inicio</a>
+           </div>
+         </div>  
+         </div>   
+         `
+       })
+     }
+   }   
+ //Manejo de la función filtrar
+ document.getElementById("select-type-pokemon").addEventListener("change",(evento)=>{
+   evento.preventDefault();
+   let type = document.getElementById("select-type-pokemon").value;
+       showPokemons(window.pokemones.filterData(allPokemon.pokemon,type));
+       clickOnImg(window.pokemones.filterData(allPokemon.pokemon,type));
   })
- }
- if (document.getElementById("option-order").value === "small-big" || document.getElementById("option-order").value === "big-small"){
-  fetch ("data/pokemon/pokemon.json")
-  .then(data => data.json())
-  .then(data=>{
+ //visualización de la data como array para función filter
+   function showPokemons(data){
+     content.innerHTML="";
+       if (Array.isArray(data)){
+         for (let valor of data){
+           content.innerHTML +=`
+             <div class="col s12 m3">
+               <div id="card1" class="card">
+                 <div  id="images" class="card-image">
+                   <img class="img-pokemon responsive-img" src= "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${valor.num}.png">
+                 </div>
+                 <div class="card-content">
+                   <p class="pokenumber">Nº ${valor.num}</p>
+                   <span class="card-title ">${valor.name}</span>
+                   <span class="tagsize ${valor.type[0]}">${valor.type[0]}</span>
+                   <span class="tagsize ${valor.type[1]}">${valor.type[1]}</span>
+                 </div>
+               </div>
+             </div> 
+           `
+       }
+       }
+   }
+   //Manejo de la función ordenar
+   document.getElementById("option-order").addEventListener("change", () => {
+     content.innerHTML = "";
+     if (document.getElementById("option-order").value === "az" || document.getElementById("option-order").value === "za"){
+         for (let valor of window.pokemones.sortData(allPokemon.pokemon,"name",document.getElementById("option-order").value)){
+           content.innerHTML +=`
+             <div class="col s12 m3">
+               <div id="card1" class="card">
+                 <div  id="images" class="card-image">
+                   <img class="img-pokemon responsive-img" src= "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${valor.num}.png">
+                 </div>
+                 <div class="card-content">
+                   <p>#${valor.num}</p>
+                   <span class="card-title">${valor.name}</span>
+                   <span class="tagsize ${valor.type[0]}">${valor.type[0]}</span>
+                   <span class="tagsize ${valor.type[1]}">${valor.type[1]}</span>
+                 </div>
+               </div>
+             </div>
+           ` 
+          clickOnImg(window.pokemones.sortData(allPokemon.pokemon,"name",document.getElementById("option-order").value));
+     }
+     }
+     if (document.getElementById("option-order").value === "small-big" || document.getElementById("option-order").value === "big-small"){
+         for (let valor of window.pokemones.sortData(allPokemon.pokemon,"num",document.getElementById("option-order").value)){
+           content.innerHTML +=`
+            <div class="col s12 m3">
+               <div id="card1" class="card">
+                 <div  id="images"  class="card-image">
+                  <img class="img-pokemon responsive-img" src= "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${valor.num}.png">
+                 </div>
+                 <div class="card-content">
+                   <p>#${valor.num}</p>
+                   <span class="card-title">${valor.name}</span>
+                   <span class="tagsize ${valor.type[0]}">${valor.type[0]}</span>
+                   <span class="tagsize ${valor.type[1]}">${valor.type[1]}</span>
+                </div>
+              </div>
+            </div>
+       `
+       clickOnImg(window.pokemones.sortData(allPokemon.pokemon,"num",document.getElementById("option-order").value));
+  }
+  }
+  })
+  function runData () {
+    
+    showPokemons(allPokemon.pokemon) 
+    clickOnImg(allPokemon.pokemon)
+  }
  
-    for (let valor of window.pokemones.sortData(data.pokemon,"num",document.getElementById("option-order").value)){
-      //console.log(valor.img)
-      contenido.innerHTML +=`
-        <div class="col s6 m3">
-          <div class="card">
-            <div class="card-image">
-              <img class="responsive-img" src= "${valor.img}">
-            </div>
-            <div class="card-content">
-              <p>#${valor.num}</p>
-              <span class="card-title">${valor.name}</span>
-              <p>${valor.type}</p>
-            </div>
-          </div>
-        </div>
-      `
- }
- })
- }
- })
-
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,43 +1,234 @@
-require('../src/data.js');
-
+require('../src/data/pokemon/pokemon.js');
 //intentando test
+
+const dataTest = window.POKEMON.pokemon;
+
 describe('pokemon', () => {
   it('Debería ser un objeto', () => {
-  expect(typeof window.pokemones).toBe('object');
+    assert.equal(typeof window.pokemones, 'object');
   });
   
-    
-  describe('pokemon.filterData', () => {
-    it('Debería ser una función', () => {
-    expect(typeof window.pokemones.filterData).toBe('function');
-    });
-  
+//testeo funcion filterData
+ describe('pokemon.filterData', () => {
+ it('Debería ser una función', () => {
+  assert.equal(typeof window.pokemones.filterData,'function');
+ });
 
+it('Debería retornar el primer pokemon de tipo planta', () => {
+let pokemonesGrass= window.pokemones.filterData(dataTest,'Grass')[0].name
+
+assert.equal(pokemonesGrass, 'Bulbasaur')
+ ;});
+
+ it('Debería retornar el segundo pokemon de tipo Fantasma', () => {
+  let pokemonesGhost= window.pokemones.filterData(dataTest,'Ghost')[1].name
+  
+  assert.equal(pokemonesGhost, 'Haunter')
+   ;});});
+  
+//testeo funcion sortData
+ describe('pokemon.sortData', () => {
+  it('Debería ser una función', () => {
+    assert.equal(typeof window.pokemones.sortData,'function');
+  });
+  
+  it('Debería retornar un arreglo de objetos con los pokemones ordenados por name de forma A-Z', ()   => {
+
+    let orderA_Z = window.pokemones.sortData(dataTest, 'name','az')
+
+    assert.deepEqual([orderA_Z[0].name, orderA_Z[1].name, orderA_Z[2].name, orderA_Z[3].name, orderA_Z[4].name],['Abra','Aerodactyl','Alakazam','Arbok','Arcanine']);
+  });
+  
+ it('Debería retornar un arreglo de objetos con los pokemones ordenados por name de forma Z-A', ()   => {
+
+    let orderZ_A = window.pokemones.sortData(dataTest, 'name','za')
+
+    assert.deepEqual([orderZ_A[0].name, orderZ_A[1].name, orderZ_A[2].name, orderZ_A[3].name],['Zubat','Zapdos','Wigglytuff','Weezing']);
   });
 
+  it('Debería retornar un arreglo de objetos con los pokemones ordenados por id de big-small', ()   => {
+
+    let numberMayor = window.pokemones.sortData(dataTest, 'num','big-small')
+
+    assert.deepEqual([numberMayor[0].id, numberMayor[1].id, numberMayor[2].id, numberMayor[3].id],[151,150,149,148]);
+  });
+
+  it('Debería retornar un arreglo de objetos con los pokemones ordenados por id de small-big', ()   => {
+
+    let numberMenor = window.pokemones.sortData(dataTest, 'num','small-big')
+
+    assert.deepEqual([numberMenor[0].id, numberMenor[1].id, numberMenor[2].id, numberMenor[3].id],[1,2,3,4]);
+  });
+
+})})
+
+//const dataOrderTest = [
+// {
+//     "id": 1,
+//     "num": "001",
+//     "name": "Bulbasaur",
+//     "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+//     "type": [
+//       "Grass",
+//       "Poison"
+//     ],
+//     "height": "0.71 m",
+//     "weight": "6.9 kg",
+//     "candy": "Bulbasaur Candy",
+//     "candy_count": 25,
+//     "egg": "2 km",
+//     "spawn_chance": 0.69,
+//     "avg_spawns": 69,
+//     "spawn_time": "20:00",
+//     "multipliers": [1.58],
+//     "weaknesses": [
+//       "Fire",
+//       "Ice",
+//       "Flying",
+//       "Psychic"
+//     ],
+//     "next_evolution": [{
+//       "num": "002",
+//       "name": "Ivysaur"
+//     }, {
+//       "num": "003",
+//       "name": "Venusaur"
+//     }]
+//   }, {
+//     "id": 2,
+//     "num": "002",
+//     "name": "Ivysaur",
+//     "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
+//     "type": [
+//       "Grass",
+//       "Poison"
+//     ],
+//     "height": "0.99 m",
+//     "weight": "13.0 kg",
+//     "candy": "Bulbasaur Candy",
+//     "candy_count": 100,
+//     "egg": "Not in Eggs",
+//     "spawn_chance": 0.042,
+//     "avg_spawns": 4.2,
+//     "spawn_time": "07:00",
+//     "multipliers": [
+//       1.2,
+//       1.6
+//     ],
+//     "weaknesses": [
+//       "Fire",
+//       "Ice",
+//       "Flying",
+//       "Psychic"
+//     ],
+//     "prev_evolution": [{
+//       "num": "001",
+//       "name": "Bulbasaur"
+//     }],
+//     "next_evolution": [{
+//       "num": "003",
+//       "name": "Venusaur"
+//     }]
+//   }, {
+//     "id": 3,
+//     "num": "003",
+//     "name": "Venusaur",
+//     "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
+//     "type": [
+//       "Grass",
+//       "Poison"
+//     ],
+//     "height": "2.01 m",
+//     "weight": "100.0 kg",
+//     "candy": "Bulbasaur Candy",
+//     "egg": "Not in Eggs",
+//     "spawn_chance": 0.017,
+//     "avg_spawns": 1.7,
+//     "spawn_time": "11:30",
+//     "multipliers": null,
+//     "weaknesses": [
+//       "Fire",
+//       "Ice",
+//       "Flying",
+//       "Psychic"
+//     ],
+//     "prev_evolution": [{
+//       "num": "001",
+//       "name": "Bulbasaur"
+//     }, {
+//       "num": "002",
+//       "name": "Ivysaur"
+//     }]
+//   }, {
+//     "id": 4,
+//     "num": "004",
+//     "name": "Charmander",
+//     "img": "http://www.serebii.net/pokemongo/pokemon/004.png",
+//     "type": [
+//       "Fire"
+//     ],
+//     "height": "0.61 m",
+//     "weight": "8.5 kg",
+//     "candy": "Charmander Candy",
+//     "candy_count": 25,
+//     "egg": "2 km",
+//     "spawn_chance": 0.253,
+//     "avg_spawns": 25.3,
+//     "spawn_time": "08:45",
+//     "multipliers": [1.65],
+//     "weaknesses": [
+//       "Water",
+//       "Ground",
+//       "Rock"
+//     ],
+//     "next_evolution": [{
+//       "num": "005",
+//       "name": "Armeleon"
+//     }, {
+//       "num": "006",
+//       "name": "Charizard"
+//     }]
+//   }
+// ]
+//
+
+
+//    it('Debería retornar un arreglo de objetos con los pokemones ordenados por //"name" de forma Z-A', () => {
+//      assert.equal(window.pokemones.sortData('az',['Dratini','Mew', 'Venasaur', //'Bulbasaur', 'Tentacruel', 'Spearow'],['Bulbasaur', 'Dratini', 'Mew', //'Spearow', 'Tentacruel', 'Venasaur']));})
+//    
+
+//    it('Debería retornar un arreglo de objetos con los pokemones ordenados por num //de forma menor a mayor', () => {
+//      assert.equal(window.pokemones.sortData('small-big',[11,150,100,56,24,110],//[11,24,56,100,110,150]));
+//    });
+//
+//  it('Debería retornar un arreglo de objetos con los pokemones ordenados por num de ////forma mayor a menor', () => {
+//    assert.equal(window.pokemones.sortData('big-small',[15,1,37,151,132,2,9,92,89], //[1,2,9,15,37,89,92,132,151])); }); });                   
+//    });
+  // });
+
+
+
+/*
+describe('pokemon.filterData', () => {
+  it('Debería ser una función', () => {
+  expect(typeof window.pokemones.filterData).toBe('function');
+  });
+  
+  it('Debería retornar un array de objetos con los pokemones que sean de tipo Planta', () => {
+  let pokemonesGrass = [];
+  for (let i=0; i<window.pokemones.filterData(window.POKEMON.pokemon,'Grass').length; i++) {
+  pokemonesGrass.push(window.pokemones.filterData(window.POKEMON.pokemon,'Grass')[i])
+  return pokemonesGrass;}
+  
+  expect(pokemonesGrass).toEqual(['Bulbasaur', 'Ivysaur', 'Venusaur','Oddish', 'Gloom', 'Vileplume', 'Paras','Parasect', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Exeggcute', 'Exeggutor','Tangela'])
+  })
+  });
+  //testeo funcion sortData
   describe('pokemon.sortData', () => {
-    it('Debería ser una función', () => {
-    expect(typeof window.pokemones.sortData).toBe('function');
-    });
+  it('Debería ser una función', () => {
+  expect(typeof window.pokemones.sortData).toBe('function');
+  });});
+  })
 
-   it('Debería retornar un arreglo de objetos con los pokemones ordenados por "name" de forma A-Z', ()   => {
-     expect(typeof window.pokemones.sortData("az",["Wartortle", "Venasaur", "Bulbasaur", "Pinsir"], ["Bulbasaur", "Pinsir", "Venasaur", "Wartortle"]));
-    });
-
-    it('Debería retornar un arreglo de objetos con los pokemones ordenados por "name" de forma Z-A', () => {
-      expect(typeof window.pokemones.sortData("az",["Dratini","Mew", "Venasaur", "Bulbasaur", "Tentacruel", "Spearow"], ["Bulbasaur", "Dratini", "Mew", "Spearow", "Tentacruel", "Venasaur"]));
-    });
-
-    it('Debería retornar un arreglo de objetos con los pokemones ordenados por "num" de forma menor a mayor', () => {
-      expect(typeof window.pokemones.sortData("small-big",[11,150,100,56,24,110], [11,24,56,100,110,150]));
-    });
-
-    it('Debería retornar un arreglo de objetos con los pokemones ordenados por "num" de forma mayor a menor', () => {
-      expect(typeof window.pokemones.sortData("big-small",[15,1,37,151,132,2,9,92,89], [1,2,9,15,37,89,92,132,151]));
-    });
-
-
-
-
-  });
-});
+  [2,8,126,0]
+  */
