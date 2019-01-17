68bd97fc87a363963baba8f142ac8006086c38d3
olgamperez <ing.olgaperez@gmail.com>
2019-01-17 13:11:41 -0300

    agregando test para string y array

--- test/data.spec.js
+++ test/data.spec.js
@@ -13,9 +13,14 @@ describe('pokemon', () => {
   it('Debería ser un objeto', () => {
     assert.equal(typeof window.pokemones, 'object','pokemon es un objeto');
   });
- // it('Debería dar un error', () => {
-  //  assert.fail(TypeError(window.pokemones), 'string','pokemon es un objeto');
- // });
+  it('No debería no ser igual a un string', () => {
+    assert.notEqual(typeof window.pokemones, 'string', 'No es un string');
+});
+
+it('Deberia ser un array', () => {
+  Array.isArray(window.pokemones);
+});
+
 
 //testeo funcion filterData
  describe('pokemon.filterData', () => {
@@ -97,178 +102,4 @@ it('Deberia retornar 12 para la cantidad de pokemon de tipo fuego', ()=>{
 
 })})
 
-
-
-
-//guardando
-//const dataOrderTest = [
-// {
-//     "id": 1,
-//     "num": "001",
-//     "name": "Bulbasaur",
-//     "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
-//     "type": [
-//       "Grass",
-//       "Poison"
-//     ],
-//     "height": "0.71 m",
-//     "weight": "6.9 kg",
-//     "candy": "Bulbasaur Candy",
-//     "candy_count": 25,
-//     "egg": "2 km",
-//     "spawn_chance": 0.69,
-//     "avg_spawns": 69,
-//     "spawn_time": "20:00",
-//     "multipliers": [1.58],
-//     "weaknesses": [
-//       "Fire",
-//       "Ice",
-//       "Flying",
-//       "Psychic"
-//     ],
-//     "next_evolution": [{
-//       "num": "002",
-//       "name": "Ivysaur"
-//     }, {
-//       "num": "003",
-//       "name": "Venusaur"
-//     }]
-//   }, {
-//     "id": 2,
-//     "num": "002",
-//     "name": "Ivysaur",
-//     "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
-//     "type": [
-//       "Grass",
-//       "Poison"
-//     ],
-//     "height": "0.99 m",
-//     "weight": "13.0 kg",
-//     "candy": "Bulbasaur Candy",
-//     "candy_count": 100,
-//     "egg": "Not in Eggs",
-//     "spawn_chance": 0.042,
-//     "avg_spawns": 4.2,
-//     "spawn_time": "07:00",
-//     "multipliers": [
-//       1.2,
-//       1.6
-//     ],
-//     "weaknesses": [
-//       "Fire",
-//       "Ice",
-//       "Flying",
-//       "Psychic"
-//     ],
-//     "prev_evolution": [{
-//       "num": "001",
-//       "name": "Bulbasaur"
-//     }],
-//     "next_evolution": [{
-//       "num": "003",
-//       "name": "Venusaur"
-//     }]
-//   }, {
-//     "id": 3,
-//     "num": "003",
-//     "name": "Venusaur",
-//     "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
-//     "type": [
-//       "Grass",
-//       "Poison"
-//     ],
-//     "height": "2.01 m",
-//     "weight": "100.0 kg",
-//     "candy": "Bulbasaur Candy",
-//     "egg": "Not in Eggs",
-//     "spawn_chance": 0.017,
-//     "avg_spawns": 1.7,
-//     "spawn_time": "11:30",
-//     "multipliers": null,
-//     "weaknesses": [
-//       "Fire",
-//       "Ice",
-//       "Flying",
-//       "Psychic"
-//     ],
-//     "prev_evolution": [{
-//       "num": "001",
-//       "name": "Bulbasaur"
-//     }, {
-//       "num": "002",
-//       "name": "Ivysaur"
-//     }]
-//   }, {
-//     "id": 4,
-//     "num": "004",
-//     "name": "Charmander",
-//     "img": "http://www.serebii.net/pokemongo/pokemon/004.png",
-//     "type": [
-//       "Fire"
-//     ],
-//     "height": "0.61 m",
-//     "weight": "8.5 kg",
-//     "candy": "Charmander Candy",
-//     "candy_count": 25,
-//     "egg": "2 km",
-//     "spawn_chance": 0.253,
-//     "avg_spawns": 25.3,
-//     "spawn_time": "08:45",
-//     "multipliers": [1.65],
-//     "weaknesses": [
-//       "Water",
-//       "Ground",
-//       "Rock"
-//     ],
-//     "next_evolution": [{
-//       "num": "005",
-//       "name": "Armeleon"
-//     }, {
-//       "num": "006",
-//       "name": "Charizard"
-//     }]
-//   }
-// ]
-//
-
-
-//    it('Debería retornar un arreglo de objetos con los pokemones ordenados por //"name" de forma Z-A', () => {
-//      assert.equal(window.pokemones.sortData('az',['Dratini','Mew', 'Venasaur', //'Bulbasaur', 'Tentacruel', 'Spearow'],['Bulbasaur', 'Dratini', 'Mew', //'Spearow', 'Tentacruel', 'Venasaur']));})
-//    
-
-//    it('Debería retornar un arreglo de objetos con los pokemones ordenados por num //de forma menor a mayor', () => {
-//      assert.equal(window.pokemones.sortData('small-big',[11,150,100,56,24,110],//[11,24,56,100,110,150]));
-//    });
-//
-//  it('Debería retornar un arreglo de objetos con los pokemones ordenados por num de ////forma mayor a menor', () => {
-//    assert.equal(window.pokemones.sortData('big-small',[15,1,37,151,132,2,9,92,89], //[1,2,9,15,37,89,92,132,151])); }); });                   
-//    });
-  // });
-
-
-
-/*
-describe('pokemon.filterData', () => {
-  it('Debería ser una función', () => {
-  expect(typeof window.pokemones.filterData).toBe('function');
-  });
-  
-  it('Debería retornar un array de objetos con los pokemones que sean de tipo Planta', () => {
-  let pokemonesGrass = [];
-  for (let i=0; i<window.pokemones.filterData(window.POKEMON.pokemon,'Grass').length; i++) {
-  pokemonesGrass.push(window.pokemones.filterData(window.POKEMON.pokemon,'Grass')[i])
-  return pokemonesGrass;}
-  
-  expect(pokemonesGrass).toEqual(['Bulbasaur', 'Ivysaur', 'Venusaur','Oddish', 'Gloom', 'Vileplume', 'Paras','Parasect', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Exeggcute', 'Exeggutor','Tangela'])
-  })
-  });
-  //testeo funcion sortData
-  describe('pokemon.sortData', () => {
-  it('Debería ser una función', () => {
-  expect(typeof window.pokemones.sortData).toBe('function');
-  });});
-  })
-
-  [2,8,126,0]
-  */
 
